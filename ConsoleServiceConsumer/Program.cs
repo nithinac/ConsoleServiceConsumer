795342fe075cdccf3d697ed8636abf9e7350bbdf
nithinac <nithinnayagamac@gmail.com>
2015-08-19 13:31:13 +0530

    Initial Commit

---
+++ ConsoleServiceConsumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using ConsoleServiceConsumer.localhost;
+
+namespace ConsoleServiceConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service service = new Service();
+            Console.WriteLine(service.HelloWorld());        
+            Console.Read();
+        }
+    }
+}
